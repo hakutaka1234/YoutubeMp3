51e85fbc63f43820acf34d1fa2013ed3c5f17d3b
hakutaka1234 <63560321+hakutaka1234@users.noreply.github.com>
2021-03-05 12:37:16 +0700

    Add files via upload

---
+++ YT.php
@@ -0,0 +1,10 @@
+<?php
+system("clear");
+echo " \33[1;31mMasukan Pilihan 
+[1] YOUTUBE TO MP3
+[2] YOUTUBE TO MP4
+";
+echo "\33[1;33mInput >> ";
+$balmond=trim(fgets(STDIN));
+if($balmond=="1"){
+include(".yt/mp3.php");}
