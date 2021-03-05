a77fb64dfa4845d7dbead21c1c2517f0ae424045
hakutaka1234 <63560321+hakutaka1234@users.noreply.github.com>
2021-03-06 01:29:36 +0700

    Add files via upload

---
+++ .yt/src.php
@@ -0,0 +1,28 @@
+<?php
+
+$WarnaRandom = array(
+"bg.mp4",
+"nsjs.mp4",
+"najsb.mp4",
+"iwowowj.mp4",
+"bdbb.mp4",
+"jsjsk.mp4",
+"heuwj.mp4",
+"hsbsb.mp4",
+"jsjsbs.mp4",
+"jsjsh.mp4",
+"hhdbd.mp4",
+"dbfbnd.mp4",
+"jsjsk.mp4",
+"hsbdb.mp4",
+"jdjsjs.mp4",
+"jsjsbb.mp4",
+"jdhdb.mp4",
+"jdjdb.mp4",
+"jsjsjsk.mp4",
+"jjw.mp4",
+"jwkshd.mp4",
+"jfbdb.mp4",
+"kslsjs.mp4"
+);
+$MEKISOURCE = $WarnaRandom[mt_rand(0, sizeof($WarnaRandom) - 1)];
